a8e1b906eaef6937a3e4f908c4732a40e1a4a35b
aram rafeq <aramrafeq2@gmail.com>
2019-07-18 11:33:38 +0300

    webpack switched to awesome parceljs

---
+++ example/example.js
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Carousel from './../src/carousel';
+import './../src/carousel.css'
+import { Image } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+const App = () => {
+  let elements = [
+    {
+      render: () => {
+        return (
+          <Image src="https://i.pinimg.com/originals/09/6d/f0/096df0eb195b8f0d9475924f9a1e9425.jpg" />
+        );
+      }
+    },
+    {
+      render: () => {
+        return <Image src="https://i.imgur.com/0eRe75Y.jpg" />;
+      }
+    },
+    {
+      render: () => {
+        return (
+          <Image src="https://flipwallpapers.com/wallpapers/anime-wallpaper-hd-resolution-For-desktop-Wallpaper.jpg" />
+        );
+      }
+    }
+  ];
+  return (
+    <div style={{ width: 500 }}>
+      <Carousel
+        elements={elements}
+        duration={3000}
+        animation="slide left"
+        showNextPrev={false}
+        showIndicators={true}
+      />
+    </div>
+  );
+};
+ReactDOM.render(<App />, document.getElementById("app"));
